3d68269577b38061b4f7f4cb2257a55f1b44d975
raaynaldo <raaynaldo@hotmail.com>
2021-04-30 11:03:12 -0400

    feat: call axios in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import { DefaultSeo } from 'next-seo';
 import SEO from '@/next-seo.config';
 import '@/styles/globals.css';
+import axios from './axios';
 
 function MyApp({ Component, pageProps }) {
+  axios();
   return (
     <>
       <DefaultSeo {...SEO} />
