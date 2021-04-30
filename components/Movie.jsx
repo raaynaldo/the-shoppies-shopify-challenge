0eb5559ce0408a1996a87036b1571851013ffaf0
raaynaldo <raaynaldo@hotmail.com>
2021-04-30 14:35:27 -0400

    feat: nomination ui for dekstop

--- components/Movie.jsx
+++ components/Movie.jsx
@@ -15,7 +15,7 @@ export default function Movie() {
         </figure>
 
         <div className='absolute top-0 flex flex-col items-center justify-center w-full h-full space-y-4 text-white transition duration-500 ease-in-out bg-gray-800 bg-opacity-75 rounded-lg opacity-0 hover:opacity-100'>
-          <div className='w-3/4 text-center'>
+          <div className='w-3/4 text-sm text-center'>
             Spiderman asda asdas asdas dsa asdassdas asdasdasdas
           </div>
           <button class='px-2 py-1 border-red-500 border rounded-lg text-xs'>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import { NextSeo } from 'next-seo';
 import CustomLink from '@/components/CustomLink';
+import Movie from '@/components/Movie';
 
 export default function Home() {
   return (
@@ -7,10 +8,16 @@ export default function Home() {
       <NextSeo />
 
       <main>
-        <section className='bg-gray-50'>
-          <div className='flex flex-col items-center min-h-screen py-5 layout'>
-            <h4>The Shoppies</h4>
-            <Search />
+        <section className='bg-white'>
+          <div className='flex justify-center min-h-screen py-5 space-x-5 layout'>
+            <div className='flex flex-col w-3/4 space-y-5'>
+              <div className=''>
+                <h3>The Shoppies</h3>
+              </div>
+              <Search />
+              <Results />
+            </div>
+            <Nominations />
           </div>
         </section>
       </main>
@@ -20,12 +27,64 @@ export default function Home() {
 
 const Search = () => {
   return (
-    <div>
-      <input type='text' className='border-gray-300 rounded-xl' />
+    <div className=''>
+      <input
+        type='text'
+        className='w-full border-gray-300 rounded-xl'
+        placeholder='Search Movie'
+      />
     </div>
   );
 };
 
 const Results = () => {
-  return <div></div>;
+  return (
+    <div className='grid grid-cols-2 gap-8 md:grid-cols-4 md:gap-10'>
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+    </div>
+  );
+};
+
+// const Nominations = () => {
+//   return <div className='fixed bottom-0 right-0 w-1/6 bg-red-300 h-1/2'></div>;
+// };
+
+const Nominations = () => {
+  return (
+    <div className='flex-1 hidden p-3 space-y-3 bg-gray-100 rounded-md h-1/2 md:block'>
+      <h4>Nominations</h4>
+      <ul className='space-y-3'>
+        <li>
+          hello asdas asdas adsada asdas asdsadas sa as{' '}
+          <button className='border'>remove</button>
+        </li>
+        <li>
+          hello <button className='border'>remove</button>
+        </li>
+        <li>
+          hello <button className='border'>remove</button>
+        </li>
+        <li>
+          hello <button className='border'>remove</button>
+        </li>
+      </ul>
+    </div>
+
+    // <div className='fixed bottom-0 left-0 w-full p-3 space-y-3 bg-gray-100 rounded-md h-1/4 md:block'>
+    //   <h4>Nominations</h4>
+    //   <ul className='space-y-3'>
+    //     <li>hello asdas asdas adsada asdas asdsadas sa as</li>
+    //     <li>hello</li>
+    //     <li>hello</li>
+    //     <li>hello</li>
+    //   </ul>
+    // </div>
+  );
 };
